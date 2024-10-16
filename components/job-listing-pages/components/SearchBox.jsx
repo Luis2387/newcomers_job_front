d385e5d3b422c16c6461704202b6ac0ae499dc8f
Luis2387 <l.montesacuna@student.fdu.edu>
2024-10-16 07:03:46 -0700

    update searchbox to search within joblist

--- components/job-listing-pages/components/SearchBox.jsx
+++ components/job-listing-pages/components/SearchBox.jsx
@@ -1,35 +1,63 @@
-'use client'
+'use client';
 
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useRouter } from "next/navigation";
 import { addKeyword } from "../../../features/filter/filterSlice";
 
 const SearchBox = () => {
     const { jobList } = useSelector((state) => state.filter);
-    const [getKeyWord, setkeyWord] = useState(jobList.keyword);
-    const dispath = useDispatch();
+    const [getKeyWord, setKeyWord] = useState(jobList.keyword);
+    const dispatch = useDispatch();
+    const router = useRouter();
 
     // keyword handler
     const keywordHandler = (e) => {
-        dispath(addKeyword(e.target.value));
+        setKeyWord(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (getKeyWord) {
+            dispatch(addKeyword(getKeyWord));
+            router.push(`/job-list?q=${getKeyWord}`);
+        }
+    };
+
+    const handleClick = () => {
+        if (getKeyWord) {
+            dispatch(addKeyword(getKeyWord));
+            router.push(`/job-list?q=${getKeyWord}`);
+        }
     };
 
     useEffect(() => {
-        setkeyWord(jobList.keyword);
-    }, [setkeyWord, jobList]);
+        setKeyWord(jobList.keyword);
+    }, [jobList]);
 
     return (
-        <>
-            <input
-                type="text"
-                name="listing-search"
-                placeholder="Job title, keywords, or company"
-                value={getKeyWord}
-                onChange={keywordHandler}
-            />
-            <span className="icon flaticon-search-3"></span>
-        </>
+        <form onSubmit={handleSubmit}>
+            <div className="search-input-container">
+                <input
+                    type="text"
+                    name="listing-search"
+                    placeholder="Job title, keywords, or company"
+                    value={getKeyWord}
+                    onChange={keywordHandler}
+                    className="search-input"
+                />
+                <button 
+                    type="submit" 
+                    className="search-btn"
+                    onClick={handleClick}
+                >
+                    <span className="icon flaticon-search-3"></span>
+                </button>
+            </div>
+        </form>
     );
 };
 
 export default SearchBox;
+
+
