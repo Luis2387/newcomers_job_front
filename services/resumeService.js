a85781c51f6158db0b5b6e32d5da01f467fb5b8c
Luis2387 <l.montesacuna@student.fdu.edu>
2024-11-26 20:35:48 -0800

    fix bug on resume

--- services/resumeService.js
+++ services/resumeService.js
@@ -1,21 +1,16 @@
-import axios from "axios";
 import { axiosInstance } from "@/services/authService";
-const API_BASE_URL = "http://localhost:8000/api";
 
-const getResume = async (id = null) => {
-  const endpoint = id ? `/resume/${id}/` : `/resume/`;
-  const response = id
-    ? await axios.get(`${API_BASE_URL}${endpoint}`) // Sin autenticación
-    : await axiosInstance.get(endpoint); // Con autenticación
-  return response.data;
+const ResumeService = {
+  getResume: async (id = null) => {
+    const endpoint = id ? `/resume/${id}/` : `/resume/`;
+    const response = await axiosInstance.get(endpoint);
+    return response.data;
+  },
+
+  updateResume: async (id, data) => {
+    const response = await axiosInstance.put(`/resume/`, data);
+    return response.data;
+  },
 };
 
-const updateResume = async (data) => {
-  const response = await axiosInstance.put(`/resume/`, data);
-  return response.data;
-};
-
-export default {
-  getResume,
-  updateResume,
-};
+export default ResumeService;
